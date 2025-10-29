89dc75fc0c8da07273182904c0d36cf630ce5e0f
aakash <aakashdeep01971@gmail.com>
2025-10-29 22:08:32 +0530

    Initial commit - anime gif app

---
+++ api/random-gif.js
@@ -0,0 +1,29 @@
+export default async function handler(req, res) {
+	try {
+		const sources = [
+			"https://api.waifu.pics/sfw/waifu",
+			"https://api.waifu.pics/sfw/smile",
+			"https://api.waifu.pics/sfw/wave",
+			"https://api.catboys.com/img",
+			"https://nekos.best/api/v2/nekos",
+		];
+		const selectedSource = sources[Math.floor(Math.random() * sources.length)];
+		const response = await fetch(selectedSource);
+		const data = await response.json();
+
+		let gifUrl =
+			data.url ||
+			(data.result && data.result[0]?.url) ||
+			data.image ||
+			"https://media.tenor.com/AlUkiGkR2j8AAAAC/anime-hello.gif";
+
+		res.writeHead(302, { Location: gifUrl });
+		res.end();
+	} catch (error) {
+		console.error("Error fetching anime gif:", error);
+		res.writeHead(302, {
+			Location: "https://media.tenor.com/AlUkiGkR2j8AAAAC/anime-hello.gif",
+		});
+		res.end();
+	}
+}
